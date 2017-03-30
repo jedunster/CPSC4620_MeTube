3d3596db25ae10c1d3a848f4732eb43e3c5656ff
Joshua Dunster <jdunste@clemson.edu>
2017-03-30 00:12:13 -0400

    Added functions in function.php to change an account password, update account metadata, update media metadata, remove keywords, add a keyword, add a comment on a media, remove a comment from a media, and edit a comment on a media. These were tested through the interactive php terminal and seem to work well with the database. They are not attached to the front end at all yet.

--- function.php
+++ function.php
@@ -1,6 +1,13 @@
 <?php
 include "mysqlClass.inc.php";
 
+//Gets the number of rows matched by the last MySQL
+//query sent to the database given by $link.
+function get_matched_rows($link)
+{
+    return preg_match("!\d+!", mysqli_info($link));
+}
+
 // Check if the given username exists in the database.
 // Return codes:    0 - User does not exist
 //                  1 - User does exist
@@ -39,6 +46,7 @@ function add_account_to_db($username, $password)
     {
         if($userExists == 0)
         {
+            //Hash the password before storing it so it is not in plaintext
 			$hash = password_hash($password, PASSWORD_DEFAULT);
 			
             if($query = mysqli_prepare(db_connect_id(), "INSERT INTO account (username, password) VALUES (?, ?)"))
@@ -64,7 +72,11 @@ function add_account_to_db($username, $password)
 	}
 }
 
-
+//Checks to see whether the supplied password is the correct one for the
+//given username. Dies if it cannot connect to the database.
+//Return codes:     0 - Correct password provided
+//                  1 - Account does not exist
+//                  2 - Incorrect password provided
 function user_pass_check($username, $password)
 {
     if($query = mysqli_prepare(db_connect_id(), "SELECT password FROM account WHERE username=?"))
@@ -78,11 +90,17 @@ function user_pass_check($username, $password)
 		die ("user_pass_check() failed. Could not query the database: <br />". mysqli_error());
     }
     
-    if (!$result or !mysqli_stmt_fetch($query))
+    if (!$result)
     {
         mysqli_stmt_close($query);
 	    die ("user_pass_check() failed. Could not query the database: <br />". mysqli_error( db_connect_id() ));
-	}
+    }
+
+    if(!mysqli_stmt_fetch($query))
+    {
+        mysqli_stmt_close($query);
+        return 1;
+    }
     else
     {
         mysqli_stmt_close($query);
@@ -91,6 +109,42 @@ function user_pass_check($username, $password)
 		else
 			return 2; //Wrong
 	}
+}
+
+//Updates the given user's password to the given new password, as long as the
+//correct current password is supplied. Dies if it cannot connect to the database.
+//Return Codes:     0 - Password successfully changed
+//                  1 - Account does not exist
+//                  2 - Wrong current password provided
+function update_user_pass($username, $currpassword, $newpassword)
+{
+    $passcheck = user_pass_check($username, $currpassword);
+    if($passcheck != 0)
+        return $passcheck;
+    
+    //Hash the password before storing it so it is not in plaintext
+	$hash = password_hash($newpassword, PASSWORD_DEFAULT);
+        
+    if($query = mysqli_prepare(db_connect_id(), "UPDATE account SET password=? WHERE username=?"))
+    {
+        mysqli_stmt_bind_param($query, "ss", $hash, $username);
+        $result = mysqli_stmt_execute($query);
+        $matched = get_matched_rows(db_connect_id());
+        mysqli_stmt_close($query);
+        if (!$result || $matched < 1)
+	    {
+	        die("update_user_pass() failed. Could not query the database: <br />". mysqli_error( db_connect_id() ));
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    else
+    {
+	    die("update_user_pass() failed. Could not query the database: <br />". mysqli_error( db_connect_id() ));
+    }
+
 }
 
 //Sets the last access time for the given media to now
@@ -137,14 +191,102 @@ function upload_error($result)
 
 //Change the summary and email for an existing account given by username. Returns true
 //on a successful change, false on an unsuccessful change.
-function update_account_information($username, $summary, $email)
+function update_account_info($username, $summary, $email)
 {
     if($query = mysqli_prepare(db_connect_id(), "UPDATE account SET summary=?, email=? WHERE username=?"))
     {
         mysqli_stmt_bind_param($query, "sss", $summary, $email, $username);
         $result = mysqli_stmt_execute($query);
+        $matched = get_matched_rows(db_connect_id());
         mysqli_stmt_close($query);
-        if (!$result || mysqli_affected_rows(db_connect_id()) < 1)
+        if (!$result || $matched < 1)
+	    {
+	        return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    else
+    {
+		return false;
+    }
+}
+
+//Sets the metadata for the media item with the given mediaid to have the given title,
+//description, category, and keywords. The keywords should take the form of an array of
+//strings of keywords. Returns true on a successfull change, false on an unsuccessful
+//change.
+function update_media_metadata($mediaid, $title, $description, $category, $keywords)
+{
+    if($query = mysqli_prepare(db_connect_id(), "UPDATE media SET title=?, description=?, category=? WHERE mediaid=?"))
+    {
+        mysqli_stmt_bind_param($query, "sssi", $title, $description, $category, $mediaid);
+        $result = mysqli_stmt_execute($query);
+        $matched = get_matched_rows(db_connect_id());
+        mysqli_stmt_close($query);
+        if (!$result || $matched < 1)
+	    {
+	        return false;
+        }
+    }
+    else
+    {
+		return false;
+    }
+
+    if(!delete_media_keywords($mediaid))
+        return false;
+
+    $success = true;
+    foreach($keywords as $currkeyword)
+    {
+        if(!add_media_keyword($mediaid, $currkeyword))
+            $success = false;
+    }
+
+    return $success;
+}
+
+//Adds the given keyword to the media item with the given id. Returns true on
+//success, false on failure.
+function add_media_keyword($mediaid, $keyword)
+{
+    if($query = mysqli_prepare(db_connect_id(), "INSERT INTO media_keyword (mediaid, keyword) VALUES (?, ?)"))
+    {
+        mysqli_stmt_bind_param($query, "is", $mediaid, $keyword);
+        $result = mysqli_stmt_execute($query);
+        $affected = mysqli_affected_rows(db_connect_id());
+        $errno = mysqli_errno(db_connect_id()); //Report success on error if it was just a duplicate entry warning
+        mysqli_stmt_close($query);
+        if ((!$result || $affected < 1) && ($errno != 1062))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+//Deletes all the keywords for the medai item with the given mediaid. Returns true
+//on success, false on failure.
+function delete_media_keywords($mediaid)
+{
+    if($query = mysqli_prepare(db_connect_id(), "DELETE FROM media_keyword WHERE mediaid=?"))
+    {
+        mysqli_stmt_bind_param($query, "i", $mediaid);
+        $result = mysqli_stmt_execute($query);
+        $affected = mysqli_affected_rows(db_connect_id());
+        mysqli_stmt_close($query);
+        if (!$result || $affected < 0)
 	    {
 	        return false;
         }
@@ -164,12 +306,13 @@ function update_account_information($username, $summary, $email)
 function add_comment($username, $mediaid, $message)
 {
     if($query = mysqli_prepare(db_connect_id(), "INSERT INTO comment (comment_id,
-        username, media_id, comment_date, message) VALUES (NULL, ?, ?, NOW(), ?)"))
+        username, mediaid, comment_date, message) VALUES (NULL, ?, ?, NOW(), ?)"))
     {
         mysqli_stmt_bind_param($query, "sis", $username, $mediaid, $message);
         $result = mysqli_stmt_execute($query);
+        $affected = mysqli_affected_rows(db_connect_id());
         mysqli_stmt_close($query);
-        if (!$result || mysqli_affected_rows(db_connect_id()) < 1)
+        if (!$result || $affected < 1)
 	    {
 	        return false;
         }
@@ -192,8 +335,9 @@ function remove_comment($commentid)
     {
         mysqli_stmt_bind_param($query, "i", $commentid);
         $result = mysqli_stmt_execute($query);
+        $affected = mysqli_affected_rows(db_connect_id());
         mysqli_stmt_close($query);
-        if (!$result || mysqli_affected_rows(db_connect_id()) < 1)
+        if (!$result || $affected < 1)
 	    {
 	        return false;
         }
@@ -216,8 +360,9 @@ function update_comment($commentid, $message)
     {
         mysqli_stmt_bind_param($query, "si", $message, $commentid);
         $result = mysqli_stmt_execute($query);
+        $matched = get_matched_rows(db_connect_id());
         mysqli_stmt_close($query);
-        if (!$result || mysqli_affected_rows(db_connect_id()) < 1)
+        if (!$result || $matched < 1)
 	    {
 	        return false;
         }
