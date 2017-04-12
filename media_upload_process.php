e5edfe190c3a35b710826c294271bd29cadb6247
Jacob A Murphy <jamrphy@webapp.cs.clemson.edu>
2017-04-11 23:08:28 -0400

    Fixed upload process to use correct file path

--- media_upload_process.php
+++ media_upload_process.php
@@ -44,14 +44,14 @@ else
             //insert into media table
             if($query = mysqli_prepare(db_connect_id(), "INSERT INTO media(mediaid,
                 title, username, type, path, size, upload_date, description, category) VALUES(NULL, ?, ?,
-                ?, CONCAT(?, (SELECT AUTO_INCREMENT FROM information_schema.tables
+                ?, CONCAT(?,(SELECT AUTO_INCREMENT FROM information_schema.tables
                 WHERE table_name='media'), '.', ?), ?, NOW(), ?, ?)"))
             {
                 $title = $_POST["title"];
 		$description = $_POST["description"];
 		$category = $_POST["category"];
                 mysqli_stmt_bind_param($query, "sssssiss", $title, $username,
-                    $_FILES['file']['type'], $dirfile, $pathinfo['extension'],
+                    $_FILES['file']['type'],$dirfile, $pathinfo['extension'],
                     $_FILES['file']['size'], $description, $category);
                 $insert = mysqli_stmt_execute($query)
                     or (unlink($upfile) and die("Insert into media error in media_upload_process.php " .mysqli_error(db_connect_id())))
@@ -64,7 +64,8 @@ else
 		foreach($keywords as $current){
 			add_media_keyword($mediaid, $current);
 		}	
-
+		chmod($upfile, 0644);
+            	rename($upfile, $dirfile.$mediaid.".".$pathinfo['extension']);
             }
             else
             {
@@ -72,15 +73,13 @@ else
             }
 
             $result="0";
-            $insert_id = mysqli_insert_id(db_connect_id());
-            chmod($upfile, 0644);
-            rename($upfile, $dirfile.$insert_id.".".$pathinfo['extension']);
+            
         }
     }
     else  
     {
 	
-        $result= "here"; //upload file failed; was 7 in quotes
+        $result= "7"; 
     }
 }
 
